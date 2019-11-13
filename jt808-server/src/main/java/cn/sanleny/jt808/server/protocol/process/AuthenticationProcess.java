d22603e0293afd6e7b300a3ca36709a9c5adb830
sanleny <308644773@qq.com>
2019-11-13 11:06:07 +0800

    update: 注册、鉴权 采用非对称加密

--- jt808-server/src/main/java/cn/sanleny/jt808/server/protocol/process/AuthenticationProcess.java
+++ jt808-server/src/main/java/cn/sanleny/jt808/server/protocol/process/AuthenticationProcess.java
@@ -1,10 +1,16 @@
 package cn.sanleny.jt808.server.protocol.process;
 
-import cn.sanleny.jt808.server.protocol.entity.Authentication;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.crypto.asymmetric.KeyType;
+import cn.hutool.crypto.asymmetric.RSA;
 import cn.sanleny.jt808.server.framework.constants.Jt808Constants;
 import cn.sanleny.jt808.server.framework.handler.AbstractProtocolProcess;
 import cn.sanleny.jt808.server.framework.handler.Jt808Message;
+import cn.sanleny.jt808.server.protocol.entity.Authentication;
+import cn.sanleny.jt808.server.utils.RsaKeyUtil;
 import lombok.extern.slf4j.Slf4j;
+
+import java.security.interfaces.RSAPrivateKey;
 
 /**
  * 【0x0102】终端鉴权
@@ -23,13 +29,14 @@ public class AuthenticationProcess extends AbstractProtocolProcess {
     @Override
     protected Jt808Message process(Jt808Message message) {
         if(message.getReplyCode() != Jt808Constants.RESP_SUCCESS) return message;
-        //TODO 先去除校验，假设都成功
         Authentication msg = (Authentication) message;
         log.debug(">>>终端鉴权：{}",msg);
-//        if(!StrUtil.equals("123",msg.getAuthToken())){
-//            msg.setReplyCode(Jt808Constants.RESP_FAILURE);
-//            log.error("终端鉴权失败，鉴权验证码：{}" + msg.getAuthToken());
-//        }
+        RSAPrivateKey privateKey = RsaKeyUtil.getRsaPrivateKey();
+        RSA rsa = new RSA(privateKey, null);
+        if(!StrUtil.equals(msg.getAuthToken(),rsa.encryptBase64(message.getHeader().getTerminalPhone(), KeyType.PrivateKey))){
+            msg.setReplyCode(Jt808Constants.RESP_FAILURE);
+            log.error("终端鉴权失败，鉴权验证码：{}" + msg.getAuthToken());
+        }
         return message;
     }
 }
--- jt808-server/src/main/java/cn/sanleny/jt808/server/protocol/process/RegisterProcess.java
+++ jt808-server/src/main/java/cn/sanleny/jt808/server/protocol/process/RegisterProcess.java
@@ -1,11 +1,16 @@
 package cn.sanleny.jt808.server.protocol.process;
 
+import cn.hutool.crypto.asymmetric.KeyType;
+import cn.hutool.crypto.asymmetric.RSA;
 import cn.sanleny.jt808.server.framework.constants.Jt808Constants;
 import cn.sanleny.jt808.server.framework.constants.Jt808MessageType;
 import cn.sanleny.jt808.server.framework.handler.Jt808Message;
 import cn.sanleny.jt808.server.protocol.entity.Register;
 import cn.sanleny.jt808.server.framework.handler.AbstractProtocolProcess;
+import cn.sanleny.jt808.server.utils.RsaKeyUtil;
 import lombok.extern.slf4j.Slf4j;
+
+import java.security.interfaces.RSAPrivateKey;
 
 /**
  * 【0x0100】终端注册
@@ -25,10 +30,11 @@ public class RegisterProcess extends AbstractProtocolProcess {
     protected Jt808Message process(Jt808Message message) {
         if(message.getReplyCode() != Jt808Constants.RESP_SUCCESS) return message;
         Register msg = (Register) message;
+        RSAPrivateKey privateKey = RsaKeyUtil.getRsaPrivateKey();
+        RSA rsa = new RSA(privateKey, null);
+        String tocken = rsa.encryptBase64(message.getHeader().getTerminalPhone(), KeyType.PrivateKey);
+        msg.setReplayToken(tocken);
         msg.setReplayType(Jt808MessageType.REGISTER_DOWN);
-        //TODO 注册逻辑,鉴权码暂时写死
-        msg.setReplayToken("123");
-//        log.debug("终端注册:{}",msg);
         return msg;
     }
 
